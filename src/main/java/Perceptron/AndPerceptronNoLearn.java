fb6fc231e027fd9241039436ce4c1fc6a88a7c3f
spbgzh <zjjhgzh@gmail.com>
2021-06-15 09:37:38 +0300

    update files

---
+++ src/main/java/Perceptron/AndPerceptronNoLearn.java
@@ -0,0 +1,36 @@
+package Perceptron;
+
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.Neuron;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.core.transfer.Step;
+import org.neuroph.nnet.Perceptron;
+import org.neuroph.nnet.comp.neuron.BiasNeuron;
+import org.neuroph.util.ConnectionFactory;
+
+import java.util.Arrays;
+
+public class AndPerceptronNoLearn {
+    public static void main(String[] args) {
+        DataSet data = new DataSet(2, 1);
+        data.add(new DataSetRow(new double[]{1, 1}, new double[]{Double.NaN}));
+        data.add(new DataSetRow(new double[]{1, 0}, new double[]{Double.NaN}));
+        data.add(new DataSetRow(new double[]{0, 1}, new double[]{Double.NaN}));
+        data.add(new DataSetRow(new double[]{0, 0}, new double[]{Double.NaN}));
+        NeuralNetwork myPerceptron = new Perceptron(2, 1);
+        Neuron biasNeuron = new BiasNeuron();
+        myPerceptron.getLayerAt(0).addNeuron(biasNeuron);
+        ConnectionFactory.createConnection(myPerceptron.getLayerAt(0).getNeuronAt(2),
+                myPerceptron.getLayerAt(1).getNeuronAt(0));
+        myPerceptron.setWeights(new double[]{0.25, 0.473, -0.485});
+        myPerceptron.getLayerAt(1).getNeuronAt(0).setTransferFunction(new Step());
+        myPerceptron.save("output/Perceptron/AndPerceptronNoLearn.nnet");
+        for (DataSetRow row : data.getRows()) {
+            myPerceptron.setInput(row.getInput());
+            myPerceptron.calculate();
+            double[] networkOutput = myPerceptron.getOutput();
+            System.out.println(Arrays.toString(row.getInput()) + "=" + Arrays.toString(networkOutput));
+        }
+    }
+}
---
+++ src/main/java/Perceptron/PerceptronClassifyNoLearn.java
@@ -0,0 +1,48 @@
+package Perceptron;
+
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.nnet.Perceptron;
+
+import java.util.Arrays;
+
+public class PerceptronClassifyNoLearn {
+    public static void main(String[] args) {
+        DataSet data =new DataSet(2,2);
+        data.add(new DataSetRow(new double[]{123,123},new double[]{1,1}));
+        data.add(new DataSetRow(new double[]{323,-321},new double[]{1,0}));
+        data.add(new DataSetRow(new double[]{-67,-34},new double[]{0,0}));
+        data.add(new DataSetRow(new double[]{-33,43},new double[]{0,1}));
+        NeuralNetwork myPerceptron= new Perceptron(2,2);
+        //myPerceptron.learn(data);
+        myPerceptron.setWeights(new double[]{1,0,0,1});
+        System.out.println(Arrays.toString(myPerceptron.getWeights()));
+        myPerceptron.save("output/Perceptron/PerceptronClassifyNoLearn.nnet");
+        testNeuralNetwork(myPerceptron,data);
+
+    }
+
+    public static void testNeuralNetwork(NeuralNetwork nnet, DataSet tset) {
+
+        for (DataSetRow dataRow : tset.getRows()){
+            nnet.setInput(dataRow.getInput());
+            nnet.calculate();
+            double[] networkOutput = nnet.getOutput();
+            System.out.print("Input: " + Arrays.toString(dataRow.getInput()));
+            System.out.println(" Output: " + Arrays.toString(networkOutput));
+            System.out.println(Arrays.toString(dataRow.getInput()) + "=" + posToString(networkOutput));
+        }
+    }
+
+    public static String posToString(double[] networkOutput ){
+        if((networkOutput[0]+networkOutput[1])==2){
+            return "第一象限";
+        }else if((networkOutput[0]+networkOutput[1])==0){
+            return "第三象限";
+        }else if((networkOutput[0]-networkOutput[1])==1){
+            return "第四象限";
+        }
+        return "第二象限";
+    }
+}
