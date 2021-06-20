a2fed2403573b72b8bd7887d3e991c0a8ca8343b
spbgzh <zjjhgzh@gmail.com>
2021-06-20 17:24:16 +0300

    add new file

--- src/main/java/Perceptron/AndPerceptron.java
+++ src/main/java/Perceptron/AndPerceptron.java
@@ -4,31 +4,46 @@ import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.Neuron;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
+import org.neuroph.core.events.LearningEvent;
+import org.neuroph.core.events.LearningEventListener;
+import org.neuroph.core.learning.IterativeLearning;
 import org.neuroph.core.learning.LearningRule;
 import org.neuroph.core.transfer.Step;
 import org.neuroph.nnet.Perceptron;
 import org.neuroph.nnet.comp.neuron.BiasNeuron;
+import org.neuroph.nnet.learning.PerceptronLearning;
 import org.neuroph.util.ConnectionFactory;
-
 
 import java.util.Arrays;
 
-public class AndPerceptronNoLearn {
+public class AndPerceptron {
     public static void main(String[] args) {
         DataSet data = new DataSet(2, 1);
-        data.add(new DataSetRow(new double[]{1, 1}, new double[]{Double.NaN}));
-        data.add(new DataSetRow(new double[]{1, 0}, new double[]{Double.NaN}));
-        data.add(new DataSetRow(new double[]{0, 1}, new double[]{Double.NaN}));
-        data.add(new DataSetRow(new double[]{0, 0}, new double[]{Double.NaN}));
+        data.add(new DataSetRow(new double[]{1, 1}, new double[]{1}));
+        data.add(new DataSetRow(new double[]{1, 0}, new double[]{0}));
+        data.add(new DataSetRow(new double[]{0, 1}, new double[]{0}));
+        data.add(new DataSetRow(new double[]{0, 0}, new double[]{0}));
         NeuralNetwork myPerceptron = new Perceptron(2, 1);
         Neuron biasNeuron = new BiasNeuron();
         myPerceptron.getLayerAt(0).addNeuron(biasNeuron);
         ConnectionFactory.createConnection(myPerceptron.getLayerAt(0).getNeuronAt(2),
                 myPerceptron.getLayerAt(1).getNeuronAt(0));
-        myPerceptron.setWeights(new double[]{0.25, 0.473, -0.485});
         myPerceptron.getLayerAt(1).getNeuronAt(0).setTransferFunction(new Step());
-        System.out.println(Arrays.toString(myPerceptron.getWeights()));
-        myPerceptron.save("output/Perceptron/AndPerceptronNoLearn.nnet");
+
+        LearningEventListener listener= new LearningEventListener() {
+            @Override
+            public void handleLearningEvent(LearningEvent event) {
+                IterativeLearning flag = (IterativeLearning)event.getSource();
+                System.out.println("iterate:"+flag.getCurrentIteration());
+                System.out.println(Arrays.toString(flag.getNeuralNetwork().getWeights()));
+            }
+        };
+
+        LearningRule rule = new PerceptronLearning();
+        rule.addListener(listener);
+        myPerceptron.setLearningRule(rule);
+        myPerceptron.learn(data);
+        myPerceptron.save("output/Perceptron/SimplePerceptron.nnet");
         for (DataSetRow row : data.getRows()) {
             myPerceptron.setInput(row.getInput());
             myPerceptron.calculate();
---
+++ src/main/java/Perceptron/PerceptronClassify.java
@@ -0,0 +1,7 @@
+package Perceptron;
+
+public class PerceptronClassify {
+
+
+}
+
